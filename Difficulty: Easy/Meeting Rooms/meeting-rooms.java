aa47c764b8b51e5e6960fcaa2de7e5aea33f399a
elriya <riyabhoite1311@gmail.com>
2026-02-16 22:45:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Meeting Rooms/meeting-rooms.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    static boolean canAttend(int[][] arr) {
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i][1] > arr[i + 1][0]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
