aed88470ba163be1fe923bc8ab63a7d952ac79cc
elriya <riyabhoite1311@gmail.com>
2026-04-02 23:45:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Painting the Fence/painting-the-fence.java
@@ -0,0 +1,20 @@
+class Solution {
+    int countWays(int n, int k) {
+        if (n == 0) return 0;
+        if (n == 1) return k;
+        
+        int same = k;
+        int diff = k * (k - 1);
+        int total = same + diff;
+        
+        for (int i = 3; i <= n; i++) {
+            same = diff;
+            
+            diff = total * (k - 1);
+            
+            total = same + diff;
+        }
+        
+        return total;
+    }
+}
