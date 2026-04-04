40d3ccab565d1e92b7fdef1139b40202fe751c33
elriya <riyabhoite1311@gmail.com>
2026-04-04 23:47:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Gray Code/gray-code.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<String> graycode(int n) {
+        ArrayList<String> result = new ArrayList<>();
+        result.add("0");
+        result.add("1");
+        
+        if (n == 1) return result;
+        
+        for (int i = 2; i <= n; i++) {
+            int size = result.size();
+            
+            for (int j = size - 1; j >= 0; j--) {
+                result.add("1" + result.get(j));
+            }
+            
+            for (int j = 0; j < size; j++) {
+                result.set(j, "0" + result.get(j));
+            }
+        }
+        
+        return result;
+    }
+}
